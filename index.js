f436827b5e9f2035b81b0a935d055d203f4bd711
CloudGamer131 <brian.mit234@gmail.com>
2022-02-25 13:44:16 -0500

    Updated Help with status

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const aoijs = require("aoi.js")
 const bot = new aoijs.Bot({
     token: "bot_token",
 prefix: "bot_prefix",
+mobile: true, //True or false
 
 intents: ["GUILDS", "GUILD_MESSAGES"]
 })
@@ -21,6 +22,19 @@ bot.status({
     text: "$serverCount servers!",
     type: "WATCHING",
     time: 12
+})
+
+bot.status({
+  text: "TEXT",
+  type: "PLAYING", //can be PLAYING, WATCHING, LISTENING, STREAMING, COMPETING
+  status: "idle", //can be online, idle, dnd, offline
+  time: 12
+})
+
+bot.status({
+text: "TEXT", 
+type: "STREAMING", 
+url: "URL"
 })
 
 //Ready Event
