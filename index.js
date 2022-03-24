a626313eb64ac5c5e7d49b3674de95af5a931bb7
CloudGamer131 <brian.mit234@gmail.com>
2022-03-24 07:49:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const aoijs = require("aoi.js")
 
 const bot = new aoijs.Bot({
     token: "bot_token",
-prefix: "bot_prefix",
+prefix: ["$getServerVar[prefix]"],
 mobile: true, //True or false
 
-intents: ["GUILDS", "GUILD_MESSAGES"]
+intents: "all"
 })
 
 bot.onMessage() //Allows to execute Commands
@@ -17,6 +17,26 @@ loader.load(bot.cmd, "./commands/")
 bot.cmd is object of Collections where the command data will be stored
 "./commands/" is the path of folder where all the commands' code will be present
 */
+
+
+bot.joinCommand({ 
+ channel:"$getServerVar[WelcomeChannel]", 
+ code:`$title[1;$replaceText[$replaceText[$replaceText[$replaceText[$getServerVar[WelcomeTitle];{user};$username];{user.ping};<@$authorID>];{server};$serverName];{user.tag};$userTag[$authorID]]]
+ $description[1;
+ 
+ 
+ $replaceText[$replaceText[$replaceText[$replaceText[$getServerVar[WelcomeMessage];{user};$username];{user.ping};<@$authorID>];{server};$serverName];{user.tag};$userTag[$authorID]]
+ 
+ 
+ 
+ ]
+ $color[1;RED]
+ $footer[1;Now $serverName Has $membersCount Members!;$serverIcon]
+ $addTimestamp[1]
+ 
+ $thumbnail[1;$userAvatar[$authorID]]`}) 
+bot.onJoin()
+
 
 bot.status({
     text: "$serverCount servers!",
@@ -39,6 +59,22 @@ url: "URL"
 
 //Ready Event
 bot.readyCommand({
-    channel: "",
-    code: `$log[Ready on $userTag[$clientID]]`
+    channel: '',
+    code: `$log[
+  ╭─━━━━━━━━━━━━━━━━━━─╮
+  Ready Command Code
+  Client: $userTag[$clientID]
+  Ping: $ping ms
+  Bot Creator: $username[$botOwnerID]#$discriminator[$botOwnerID]
+  Commands loaded: $commandsCount
+  ╰─━━━━━━━━━━━━━━━━━━─╯
+  ]`
 })
+
+
+bot.variables ({
+prefix: "$",
+WelcomeChannel: "",
+WelcomeMessage: "Welcome {user.ping} To {server}!",
+WelcomeTitle:"Welcome {user.ping}",
+});
